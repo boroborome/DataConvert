bd332fad5199c99bc538020f06664eebb9a37500
Yushan Gao <ysgao@thoughtworks.com>
2018-06-07 13:10:52 +0800

    Init Project

---
+++ src/main/java/com/happy3w/util/convert/convert/ConvertApplication.java
@@ -0,0 +1,12 @@
+package com.happy3w.util.convert.convert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertApplication.class, args);
+	}
+}
